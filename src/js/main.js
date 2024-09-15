4679ab6a581e92618b6ca5665d79fd9798ed0a03
Vladyslav <vladyslav.honchar1990@gmail.com>
2024-09-16 01:38:40 +0300

    Added Footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,39 @@
-import '../scss/style.scss'
-const genenrateData = () => {
-    return 1;
-};
-const getData = '';
-genenrateData();
-console.log('asdsdas')
+import Swiper from "swiper";
+import {Autoplay, Navigation, Pagination} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const menuBtn = document.querySelector('.nav-header__menu-button');
+const mobileMenu = document.querySelector('.nav-header__menu-list');
+menuBtn.addEventListener('click', handleClickMenuBtn);
+
+function handleClickMenuBtn(e) {
+    menuBtn.classList.toggle('active')
+    mobileMenu.classList.toggle('is-open')
+
+}
+
+const swiper = new Swiper('.swiper', {
+    modules:    [
+        Navigation,
+        Pagination,
+        Autoplay,
+    ],
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,  // Пагинация будет кликабельной
+    },
+    loop:       true,
+    autoplay:   {
+        delay:             5000,
+        waitForTransition: true,
+    },
+    speed:      1000,
+    direction:  'horizontal',
+    effect:     'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+
+});
